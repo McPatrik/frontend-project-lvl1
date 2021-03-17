3e2c1bf3640fd9431a03bd74fb11d62e7e15a6f9
tatiana <tstartrekt@gmail.com>
2021-03-17 17:22:06 +0300

    исправление ошибок по линтеру

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 // eslint-disable-next-line no-unused-vars
-import witam from '../src/cli.js'
+import witam from '../src/cli.js';
+
 witam();
--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable semi */
 import readlineSync from 'readline-sync';
+
 const witam = (x) => {
+  console.log('Welcome to the Brain Games!')
 
-    console.log("Welcome to the Brain Games!")
+  const name = readlineSync.question('May I have your name? ');
 
-    const name = readlineSync.question('May I have your name? ');
-
-    console.log(`Hello, ${name}`);
+  console.log(`Hello, ${name}`);
 }
+
 export default witam;
