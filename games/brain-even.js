bba4557bd7bbd452f519ee9b5963e1d50144d6e9
tatiana <tstartrekt@gmail.com>
2021-03-28 20:39:30 +0300

    создание директории для исходного кода логики игр

---
+++ games/brain-even.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+
+import {randomInteger} from '../src/index.js';
+
+export const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+export const answerS = () => {
+  const randomNumber = randomInteger();
+  const answers = [];
+  let trueAnswer;
+  if (randomNumber % 2 === 0) {
+    trueAnswer = 'yes';
+  }
+  if (randomNumber % 2 > 0) {
+    trueAnswer = 'no';
+  }
+  answers.push(trueAnswer);
+  console.log(`Question: ${randomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  console.log(userAnswer);
+  answers.push(userAnswer);
+  return answers;
+};
