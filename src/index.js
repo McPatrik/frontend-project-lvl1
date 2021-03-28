0403a81f1b871a2905518d5f532d941a23439b9f
tatiana <tstartrekt@gmail.com>
2021-03-28 20:40:22 +0300

    создание файла с общей логикой игр

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { RuleTester } from 'eslint';
+import readlineSync from 'readline-sync';
+
+export const randomInteger = (min = 1, max = 20) => {
+// случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  return name;
+};
+
+export const logicGame = (task, answers) => {
+  const name = greeting();
+  console.log(task);
+  let current = 0;
+  while (current < 3) {
+    const answerS = answers();
+    const userAnswer = answerS[1];
+    const trueAnswer = answerS[0];
+    if (trueAnswer === userAnswer) {
+      current += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.
+      Let's try again, ${name}`);
+      break;
+    }
+  }
+  if (current === 3) {
+    console.log(`Congratulations, ${name}`);
+  }
+};
